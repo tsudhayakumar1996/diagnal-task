33e1e9c38ef30d0f83b24c7b62512d345602638d
Udhay <tsudhayakumar1996@gmail.com>
2024-07-28 19:38:17 +0530

    theme added

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={titillium_web.className}>
-        <ThemeProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <PostContextProvider>{children}</PostContextProvider>
         </ThemeProvider>
       </body>
